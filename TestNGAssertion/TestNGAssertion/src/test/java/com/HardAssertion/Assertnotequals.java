7ebc4175ffa7f2de939f2b78c6197e2a96965362
Devansh bajpai <163446624+DevanshBajpai09@users.noreply.github.com>
2026-06-21 12:51:30 +0530

    Update Assertnotequals.java

--- TestNGAssertion/TestNGAssertion/src/test/java/com/HardAssertion/Assertnotequals.java
+++ TestNGAssertion/TestNGAssertion/src/test/java/com/HardAssertion/Assertnotequals.java
@@ -1,5 +1,40 @@
 package com.HardAssertion;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class Assertnotequals {
 
+    WebDriver driver;
+    String URL = "https://www.easemytrip.com/";
+
+    @BeforeTest
+    public void Setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(URL);
+    }
+
+    @Test
+    public void TestAssertions() {
+
+        String String1 = "MyAccount";
+        String String2 = "My Booking";
+
+       
+        // assertNotEquals
+        Assert.assertNotEquals(String1, String2);
+        System.out.println("assertNotEquals Passed");
+
+      
+    }
+
+    @AfterTest
+    public void TearDown() {
+        driver.quit();
+    }
 }
